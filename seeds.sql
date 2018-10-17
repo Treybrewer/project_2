4eea1697148a93b3dcc6b8e2f63b59e904df3fa5
Natalie A <nmanderson314@gmail.com>
2018-10-17 19:23:54 -0400

    changes to schema and initial seeds

---
+++ seeds.sql
@@ -0,0 +1,12 @@
+
+--photo 1 = matheson bridge
+INSERT INTO photos (user_id, marker_id, photo_url, city, date, time)
+VALUES (1,1,'./uploads/mathesonbridge.jpg','CHARLOTTE', 2018-01-01, 17:00:00)
+
+--user 1 = 
+INSERT INTO users (user_name, user_fname, user_lname, bio)
+VALUES ('shutterbug1', 'Dirk', 'Torres', 'live, laugh, love')
+
+--marker 1 = matheson bridge @35.2462189,-80.8134568
+INSERT INTO markers (name, address, lat, lng, type, city)
+VALUES ('Matheson Bridge', 'Matheson Ave, Charlotte, NC', 35.2462189, -80.8134568, 'Photo-Op', 'Charlotte')
