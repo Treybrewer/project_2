464589d5d1132afbeab2266b1c5126da241b509b
Natalie A <nmanderson314@gmail.com>
2018-10-20 13:28:03 -0400

    changes to the routes to include html-routes to allow for a landing page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,25 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
 var path = require("path");
 
-// module.exports = function(app) {};
-// app.gets
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+
+  // cms route loads cms.html
+  app.get("/profile", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/profile.html"));
+  });
+
+};
