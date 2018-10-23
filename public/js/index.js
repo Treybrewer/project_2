bf0b936434de957e92416dc4dee2df176fd42a87
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-23 16:07:39 -0400

    testing

--- public/js/index.js
+++ public/js/index.js
@@ -87,7 +87,7 @@ function initMap() {
                             lng: ${newLng}`)
 
                 var marker = new google.maps.Marker({
-                    position: { lat: newLat, lng: newLng },
+                    position: { lat: newLat, lng: -newLng },
                     map: map,
                 });
 
