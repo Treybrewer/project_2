d8126f9d0d4bdcd371f876b097828fb27a9bb4f3
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-24 14:56:47 -0400

    building post method for photo map

--- public/js/photo-info.js
+++ public/js/photo-info.js
@@ -50,100 +50,105 @@ function initMap() {
 };
 
 
+$(document).on("click", ".submit", postData);
+
+function postData() {
+    
+
+};
+
+
+// $(document).ready(function () {
+//     /* global moment */
 
 
 
-$(document).ready(function () {
-    /* global moment */
 
+//     $(document).on("click", ".submit", handleUpload);
 
+//     //global variables
+//     var newMarkerId;
+//     var newUserId;
 
+//     function handleUpload() {
+//         event.preventDefault();
 
-    $(document).on("click", ".submit", handleUpload);
+//         //function for adding userdata FIRST w/ callback for Marker
+//         //name, email, username
+//         function addUser(post, callback) {
+//             console.log("adding user data");
+//             $.ajax({
+//                 method: "POST",
+//                 url: "/api/users",
+//                 data: post
+//             }).then(function (resp) {
+//                 console.log(resp);
+//                 newUserId = resp.id;
 
-    //global variables
-    var newMarkerId;
-    var newUserId;
+//                 callback(newLocation, addPhoto);
+//             })
+//         };
 
-    function handleUpload() {
-        event.preventDefault();
+//         //capture variables
+//         var newUser = {
+//             name: $("#photogName").val().trim(),
+//             username: $("#username").val().trim(),
+//             email: $("#email").val().trim()
+//         };
 
-        //function for adding userdata FIRST w/ callback for Marker
-        //name, email, username
-        function addUser(post, callback) {
-            console.log("adding user data");
-            $.ajax({
-                method: "POST",
-                url: "/api/users",
-                data: post
-            }).then(function (resp) {
-                console.log(resp);
-                newUserId = resp.id;
+//         var locationName = "UNCC Uptown Campus";
+//         var lat = 35.2284396;
+//         var lng = 80.837108;
+//         var address = "320 E 9th St, Charlotte, NC, 28202";
+//         var city;
 
-                callback(newLocation, addPhoto);
-            })
-        };
+//         var newLocation = {
+//             name: locationName
+//                 .trim(),
+//             address: address
+//                 .trim(),
+//             type: "Photo-Op",
+//             lat: lat,
+//             lng: lng
+//         };
 
-        //capture variables
-        var newUser = {
-            name: $("#photogName").val().trim(),
-            username: $("#username").val().trim(),
-            email: $("#email").val().trim()
-        };
+//         //add user and set up call back
+//         addUser(newUser, addMarker);
+//     };
 
-        var locationName = "UNCC Uptown Campus";
-        var lat = 35.2284396;
-        var lng = 80.837108;
-        var address = "320 E 9th St, Charlotte, NC, 28202";
-        var city;
+//     function addMarker(post, callback) {
+//         //data to go into markers table  
+//         console.log('adding marker')
+//         $.ajax({
+//             method: "POST",
+//             url: "/api/markers",
+//             data: post
+//         }).then(function (resp) {
+//             // response.json(resp.get('id'))
+//             console.log(resp.id);
+//             newMarkerId = resp.id;
 
-        var newLocation = {
-            name: locationName
-                .trim(),
-            address: address
-                .trim(),
-            type: "Photo-Op",
-            lat: lat,
-            lng: lng
-        };
+//             var newPhoto = {
+//                 UserId: newUserId,
+//                 MarkerId: newMarkerId,
+//                 photo_url: "../uploads/mathesonbridge.jpg",//photoURL,
+//                 photog_notes: $("#photogNotes").val(),
+//                 date: $("#date").val(),
+//             };
 
-        //add user and set up call back
-        addUser(newUser, addMarker);
-    };
+//             callback(newPhoto);
+//         })
+//     };
 
-    function addMarker(post, callback) {
-        //data to go into markers table  
-        console.log('adding marker')
-        $.ajax({
-            method: "POST",
-            url: "/api/markers",
-            data: post
-        }).then(function (resp) {
-            // response.json(resp.get('id'))
-            console.log(resp.id);
-            newMarkerId = resp.id;
+//     function addPhoto(post) {
+//         console.log('adding photo')
+//         $.ajax({
+//             method: "POST",
+//             url: "/api/photos",
+//             data: post
+//         }).then(function (resp) {
+//             console.log(resp);
+//         })
+//     };
 
-            var newPhoto = {
-                UserId: newUserId,
-                MarkerId: newMarkerId,
-                photo_url: "../uploads/mathesonbridge.jpg",//photoURL,
-                photog_notes: $("#photogNotes").val(),
-                date: $("#date").val(),
-            };
-
-            callback(newPhoto);
-        })
-    };
-
-    function addPhoto(post) {
-        console.log('adding photo')
-        $.ajax({
-            method: "POST",
-            url: "/api/photos",
-            data: post
-        }).then(function (resp) {
-            console.log(resp);
-        })
-    };
-
-});
+// });
