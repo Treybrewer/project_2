014643ece168aa42740769dd621457f0788306d6
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-24 10:50:00 -0400

    deleting force true

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ require("./routes/photo-api-routes.js")(app);
 require("./routes/marker-api-routes.js")(app);
 
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
