5f0ee61722571b76b744fd2a6ff7535c11cd7935
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-24 10:40:31 -0400

    adjusting routes

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.static("./public"));
 require("./routes/html-routes.js")(app);
 // require("./routes/api-routes.js")(app);
 
-require("./routes/user-api-routes.js")(app);
-require("./routes/photo-api-routes.js")(app);
-require("./routes/marker-api-routes.js")(app);
+require("./routes/users-api-routes.js")(app);
+require("./routes/photos-api-routes.js")(app);
+require("./routes/markers-api-routes.js")(app);
 
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
